34ed1814bb372d17947333b4fe167f359b023a82
Perri Specter <plspecter@gmail.com>
2018-10-28 15:01:17 -0400

    burger-

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,3 +1,17 @@
 // grab our dependencies
 var express = require("express");
 var burger = require("..models/burger.js");
+
+//creating a route
+app.get("/", function (req, res) {
+
+
+    connection.query("SELECT * FROM  burger", function (err, result) {
+
+    })
+
+});
+
+// Finally we send the user the HTML file we dynamically created.
+//this si exporting everything
+res.send(html);
