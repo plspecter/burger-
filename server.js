c16fab59eb87bc8f95d142ab442ba539bc0fff25
Perri Specter <plspecter@gmail.com>
2018-10-31 13:42:27 -0400

    burger-

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ var app = express();
 var PORT = process.env.PORT || 8090;
 
 //Set up express to handle data
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //grab handlebars
 var handlebars = require("express-handlebars");
-app.engine("handlebars", handlebars({defaultLayout:"main"}));
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars")
 
 //grab mysql
@@ -23,29 +23,47 @@ var mysql = require("mysql");
 
 //this is where we call on our mysql database
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-  });
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "burgers_db"
+});
+
+connection.connect(function (err) {
+  if (err) {
+    console.log("connection error" + err.stack);
+    return;
+  }
+  console.log("connection id" + connection.threadId)
+})
+
+
 
 //Data
 var burgers = [
-{ name: "Veggie Burger", price: 6,},
-{ name: "Whopper", price: 5,},
-{ name: "Bacon King", price: 8,},
+  { name: "Veggie Burger", price: 6, },
+  { name: "Whopper", price: 5, },
+  { name: "Bacon King", price: 8, },
 ];
 
 //Routes
+app.get("index.handlebars", function (req, res) {
+  connection.query("SELECT * FROM burger_db", function (err, data) {
+    if (err) {
+      console.log("connection error" + err.stack);
+      return;
+    }
+    console.log("connection id" + connection.threadId)
+  });
 
 
+  //the first page the server will load
+  res.render("index.handlebars", { burgers: data })
+});
 
-
-
-  // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
-  
